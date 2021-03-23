f61d61de15a2fc5ee4b10a4e2290517c133fc6c3
AgiusDev <78762493+AgiusDev@users.noreply.github.com>
2021-03-23 15:57:11 -0700

    add array
    If applied, this commit will have added objects to the pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Tacos al Pastor';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]}
+  { name: "Charmander", height: 0.6, types: ["Fire"]}
+  { name: "Squirtle", height: 0.5, types: ["Water"]}
+];
